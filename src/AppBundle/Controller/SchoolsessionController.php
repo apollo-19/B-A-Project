c38ba27775137048674502acb3ff4e1823ed4068
Nahusenay21 <nahusenaytesfaye5@gmail.com>
2017-12-05 09:25:20 +0300

    Dec 04

---
+++ src/AppBundle/Controller/SchoolsessionController.php
@@ -0,0 +1,271 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\Session;
+use AppBundle\Entity\Schoolsession;
+
+class SchoolsessionController extends Controller
+{
+    /**
+     * @Route("/session", name="session_home")
+     */
+    public function indexAction()
+    {
+      $session = new Session();
+
+      if($session->get('user_name') && $session->get('user_type')){
+        return $this->render('session/index.html.twig', $data);
+      } else
+        return $this->redirectToRoute('user_signin');
+    }
+
+    /**
+     * @Route("/session/create", name="session_create")
+     */
+    public function sessionCreateAction(Request $request)
+    {
+      $session = new Session();
+
+      if($session->get('user_id') && ($session->get('user_type') == 'admin')){
+        $data = [];
+        $data['mode'] = 'new';
+
+        $form = $this ->createFormBuilder()
+                      ->add('session_code')
+                      ->add('session_name')
+                      ->add('session_section')
+                      ->add('session_teacher')
+                      ->add('course_module_type')
+                      ->add('course_module_id')
+                      ->add('session_start_date')
+                      ->add('session_end_date')
+                      ->getForm();
+
+        $section = $this->getDoctrine()
+                            ->getRepository('AppBundle:Section')
+                            ->findAll();
+
+        $data['sections'] = $section;
+
+        $teacher = $this->getDoctrine()
+                            ->getRepository('AppBundle:Teacher')
+                            ->findAll();
+
+        $data['teachers'] = $teacher;
+
+        $module = $this->getDoctrine()
+                            ->getRepository('AppBundle:Module')
+                            ->findAll();
+
+        $data['modules'] = $module;
+
+        $course = $this->getDoctrine()
+                            ->getRepository('AppBundle:Course')
+                            ->findAll();
+
+        $data['courses'] = $course;
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted()){
+          $data['form'] = [];
+          $school_session_data = $form->getData();
+          $data['form'] = $school_session_data;
+
+          $school_session = new Schoolsession();
+          $school_session->setSessionCode($school_session_data['session_code']);
+          $school_session->setSessionName($school_session_data['session_name']);
+          $school_session->setSectionId($school_session_data['session_section']);
+          $school_session->setTeacherId($school_session_data['session_teacher']);
+          $school_session->setCourseModuleType($school_session_data['course_module_type']);
+          $school_session->setCourseModuleId($school_session_data['course_module_id']);
+          $school_session->setSessionStartDate($school_session_data['session_start_date']);
+          $school_session->setSessionEndDate($school_session_data['session_end_date']);
+          $school_session->setCreatedBy($session->get('user_id'));
+
+          $em = $this->getDoctrine()->getManager();
+          $em->persist($school_session);
+          $em->flush();
+
+          return $this->redirectToRoute('session_view');
+        }
+
+        return $this->render('session/form.html.twig', $data);
+      } else {
+        $data['message'] = 'You Are Not Qualified to Create a Session.';
+        return $this->render('accessDenied.html.twig', $data);
+      }
+    }
+
+    /**
+     * @Route("/session/edit/{session_id}", name="session_edit")
+     */
+    public function sessionEditAction(Request $request, $session_id)
+    {
+      $data = [];
+      $data['mode'] = 'edit';
+
+      $form = $this ->createFormBuilder()
+                    ->add('session_code')
+                    ->add('session_name')
+                    ->add('session_section')
+                    ->add('session_teacher')
+                    ->add('course_module_type')
+                    ->add('course_module_id')
+                    ->add('session_start_date')
+                    ->add('session_end_date')
+                    ->getForm();
+
+      $school_session = $this->getDoctrine()
+                          ->getRepository('AppBundle:Schoolsession')
+                          ->findOneById($session_id);
+
+      $section = $this->getDoctrine()
+                          ->getRepository('AppBundle:Section')
+                          ->findAll();
+
+      $data['sections'] = $section;
+
+      $teacher = $this->getDoctrine()
+                          ->getRepository('AppBundle:Teacher')
+                          ->findAll();
+
+      $data['teachers'] = $teacher;
+
+      $module = $this->getDoctrine()
+                          ->getRepository('AppBundle:Module')
+                          ->findAll();
+
+      $data['modules'] = $module;
+
+      $course = $this->getDoctrine()
+                          ->getRepository('AppBundle:Course')
+                          ->findAll();
+
+      $data['courses'] = $course;
+
+      $school_session_data['session_code'] = $school_session->getSessionCode();
+      $school_session_data['session_name'] = $school_session->getSessionName();
+      $school_session_data['session_section'] = $school_session->getSectionId();
+      $school_session_data['session_teacher'] = $school_session->getTeacherId();
+      $school_session_data['course_module_type'] = $school_session->getCourseModuleType();
+      $school_session_data['course_module_id'] = $school_session->getCourseModuleId();
+      $school_session_data['session_start_date'] = $school_session->getSessionStartDate();
+      $school_session_data['session_end_date'] = $school_session->getSessionEndDate();
+
+      $session = new Session();
+
+      if($session->get('user_name') && ($session->get('user_type') == 'admin')){
+        $data['form'] = $school_session_data;
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted()){
+          $data['form'] = [];
+          $school_session_data = $form->getData();
+          $data['form'] = $school_session_data;
+
+          $school_session->setSessionCode($school_session_data['session_code']);
+          $school_session->setSessionName($school_session_data['session_name']);
+          $school_session->setSectionId($school_session_data['session_section']);
+          $school_session->setTeacherId($school_session_data['session_teacher']);
+          $school_session->setCourseModuleType($school_session_data['course_module_type']);
+          $school_session->setCourseModuleId($school_session_data['course_module_id']);
+          $school_session->setSessionStartDate($school_session_data['session_start_date']);
+          $school_session->setSessionEndDate($school_session_data['session_end_date']);
+
+          $em = $this->getDoctrine()->getManager();
+          $em->persist($school_session);
+          $em->flush();
+
+          return $this->redirectToRoute('session_view');
+        }
+        return $this->render('session/form.html.twig', $data);
+      } else {
+        $data['message'] = 'You Are Not Qualified to Edit This Session.';
+        return $this->render('accessDenied.html.twig', $data);
+      }
+    }
+
+    /**
+     * @Route("/session/delete/{session_id}", name="session_delete")
+     */
+    public function sessionDeleteAction(Request $request, $session_id)
+    {
+      $session = new Session();
+      $school_session_data = [];
+
+      $session = $this->getDoctrine()
+                          ->getRepository('AppBundle:Schoolsession')
+                          ->findOneById($session_id);
+      $school_session_data['created_by'] = $session->getCreatedBy();
+
+      if($session->get('user_name') && ($session->get('user_type') == 'admin') && ($session->get('user_id') == $school_session_data['created_by'])){
+        $session = $this->getDoctrine()
+                            ->getRepository('AppBundle:Schoolsession')
+                            ->findOneById($session_id);
+
+        $em = $this->getDoctrine()->getManager();
+
+        $em->remove($session);
+        $em->flush();
+
+        return $this->redirectToRoute('session_view');
+      } else {
+        $data['message'] = 'You Are Not Qualified to Delete This Session.';
+        return $this->render('accessDenied.html.twig', $data);
+      }
+    }
+
+    /**
+     * @Route("/session/view", name="session_view")
+     */
+    public function sessionViewAction()
+    {
+      $session = new Session();
+
+      if($session->get('user_name') && $session->get('user_type') && ($session->get('user_type') == 'admin')){
+        $data = [];
+        $data['sessions'] = [];
+
+        $session = $this->getDoctrine()
+                            ->getRepository('AppBundle:Schoolsession')
+                            ->findAll();
+
+        $data['school_sessions'] = $session;
+
+        $section = $this->getDoctrine()
+                            ->getRepository('AppBundle:Section')
+                            ->findAll();
+
+        $data['sections'] = $section;
+
+        $teacher = $this->getDoctrine()
+                            ->getRepository('AppBundle:Teacher')
+                            ->findAll();
+
+        $data['teachers'] = $teacher;
+
+        $module = $this->getDoctrine()
+                            ->getRepository('AppBundle:Module')
+                            ->findAll();
+
+        $data['modules'] = $module;
+
+        $course = $this->getDoctrine()
+                            ->getRepository('AppBundle:Course')
+                            ->findAll();
+
+        $data['courses'] = $course;
+
+        return $this->render('session/view.html.twig', $data);
+      } else {
+        $data['message'] = 'You Are Not Qualified to View Sessions.';
+        return $this->render('accessDenied.html.twig', $data);
+      }
+    }
+}
---
+++ src/AppBundle/Entity/Schoolsession.php
@@ -0,0 +1,286 @@
+<?php
+
+namespace AppBundle\Entity;
+
+/**
+ * Schoolsession
+ */
+class Schoolsession
+{
+    /**
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $sessionCode;
+
+    /**
+     * @var string
+     */
+    private $sessionName;
+
+    /**
+     * @var integer
+     */
+    private $sectionId;
+
+    /**
+     * @var integer
+     */
+    private $teacherId;
+
+    /**
+     * @var string
+     */
+    private $courseModuleType;
+
+    /**
+     * @var integer
+     */
+    private $courseModuleId;
+
+    /**
+     * @var string
+     */
+    private $sessionStartDate;
+
+    /**
+     * @var string
+     */
+    private $sessionEndDate;
+
+    /**
+     * @var integer
+     */
+    private $createdBy;
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set sessionCode
+     *
+     * @param string $sessionCode
+     *
+     * @return Schoolsession
+     */
+    public function setSessionCode($sessionCode)
+    {
+        $this->sessionCode = $sessionCode;
+
+        return $this;
+    }
+
+    /**
+     * Get sessionCode
+     *
+     * @return string
+     */
+    public function getSessionCode()
+    {
+        return $this->sessionCode;
+    }
+
+    /**
+     * Set sessionName
+     *
+     * @param string $sessionName
+     *
+     * @return Schoolsession
+     */
+    public function setSessionName($sessionName)
+    {
+        $this->sessionName = $sessionName;
+
+        return $this;
+    }
+
+    /**
+     * Get sessionName
+     *
+     * @return string
+     */
+    public function getSessionName()
+    {
+        return $this->sessionName;
+    }
+
+    /**
+     * Set sectionId
+     *
+     * @param integer $sectionId
+     *
+     * @return Schoolsession
+     */
+    public function setSectionId($sectionId)
+    {
+        $this->sectionId = $sectionId;
+
+        return $this;
+    }
+
+    /**
+     * Get sectionId
+     *
+     * @return integer
+     */
+    public function getSectionId()
+    {
+        return $this->sectionId;
+    }
+
+    /**
+     * Set teacherId
+     *
+     * @param integer $teacherId
+     *
+     * @return Schoolsession
+     */
+    public function setTeacherId($teacherId)
+    {
+        $this->teacherId = $teacherId;
+
+        return $this;
+    }
+
+    /**
+     * Get teacherId
+     *
+     * @return integer
+     */
+    public function getTeacherId()
+    {
+        return $this->teacherId;
+    }
+
+    /**
+     * Set courseModuleType
+     *
+     * @param string $courseModuleType
+     *
+     * @return Schoolsession
+     */
+    public function setCourseModuleType($courseModuleType)
+    {
+        $this->courseModuleType = $courseModuleType;
+
+        return $this;
+    }
+
+    /**
+     * Get courseModuleType
+     *
+     * @return string
+     */
+    public function getCourseModuleType()
+    {
+        return $this->courseModuleType;
+    }
+
+    /**
+     * Set courseModuleId
+     *
+     * @param integer $courseModuleId
+     *
+     * @return Schoolsession
+     */
+    public function setCourseModuleId($courseModuleId)
+    {
+        $this->courseModuleId = $courseModuleId;
+
+        return $this;
+    }
+
+    /**
+     * Get courseModuleId
+     *
+     * @return integer
+     */
+    public function getCourseModuleId()
+    {
+        return $this->courseModuleId;
+    }
+
+    /**
+     * Set sessionStartDate
+     *
+     * @param string $sessionStartDate
+     *
+     * @return Schoolsession
+     */
+    public function setSessionStartDate($sessionStartDate)
+    {
+        $this->sessionStartDate = $sessionStartDate;
+
+        return $this;
+    }
+
+    /**
+     * Get sessionStartDate
+     *
+     * @return string
+     */
+    public function getSessionStartDate()
+    {
+        return $this->sessionStartDate;
+    }
+
+    /**
+     * Set sessionEndDate
+     *
+     * @param string $sessionEndDate
+     *
+     * @return Schoolsession
+     */
+    public function setSessionEndDate($sessionEndDate)
+    {
+        $this->sessionEndDate = $sessionEndDate;
+
+        return $this;
+    }
+
+    /**
+     * Get sessionEndDate
+     *
+     * @return string
+     */
+    public function getSessionEndDate()
+    {
+        return $this->sessionEndDate;
+    }
+
+    /**
+     * Set createdBy
+     *
+     * @param integer $createdBy
+     *
+     * @return Schoolsession
+     */
+    public function setCreatedBy($createdBy)
+    {
+        $this->createdBy = $createdBy;
+
+        return $this;
+    }
+
+    /**
+     * Get createdBy
+     *
+     * @return integer
+     */
+    public function getCreatedBy()
+    {
+        return $this->createdBy;
+    }
+}
